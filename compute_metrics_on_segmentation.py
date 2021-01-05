7b5f76b218978fa3579c1583ed923dd17c4e9b4f
CrispyHarder <christian.harder96@gmail.com>
2021-01-05 14:31:18 +0100

    dimensionality reduction on frankfurt data

--- compute_metrics_on_segmentation.py
+++ compute_metrics_on_segmentation.py
@@ -1,29 +1,22 @@
+## basic sketch, nothing to see here
+
 import numpy as np
 import torchio
-from skimage.measure import label
+from skimage.measure import label, regionprops
 import os 
 from mp.paths import storage_data_path
 import math
 
-def compute_metrics(seg_path):
-    '''gets a 3D segmentation of a Covid consolidation and computes metrices on it.
-    Will be used to assess if segmentation is plausible'''
+def get_array_of_dicescores(img): 
 
-    #load image
-    img = torchio.Image(seg_path, type=torchio.LABEL)
-    img = img.numpy()
-    spacing = img.spacing
-    nr_slices = img.shape[2]
-    
-    # 1. Dice Scores
-
-    # 1.1 compute dice scores
+    shape = np.shape(img)
+    nr_slices = shape[0]
     arr_of_dicescores = np.array([])
 
-    first_slide = img[:, :, 0, :]
+    first_slide = img[0, :, :]
     first_ones = np.sum(first_slide)
     for i in range(nr_slices-1):
-        second_slide = img[:, :, i+1, :]
+        second_slide = img[i+1,:,:]
         second_ones = np.sum(second_slide)
         intersection = np.dot(first_slide.flatten(),
                                  second_slide.flatten())
@@ -41,6 +34,20 @@ def compute_metrics(seg_path):
         # update index
         first_slide = second_slide
         first_ones = second_ones
+    	return arr_of_dicescores
+
+def compute_metrics(seg_path):
+    '''gets a 3D segmentation of a Covid consolidation and computes metrices on it.
+    Will be used to assess if segmentation is plausible'''
+
+    #load image
+    img = torchio.Image(seg_path, type=torchio.LABEL)
+    img = img.numpy()
+    spacing = img.spacing
+    nr_slices = img.shape[2]
+    
+    # 1. Dice scores
+    arr_of_dicescores = get_array_of_dicescores(img)
 
     # 1.2 compute the average value of dice score
     dice_avg_value = np.average(arr_of_dicescores)
@@ -61,6 +68,14 @@ def compute_metrics(seg_path):
     # 2.2 compute connected components
     labeled_image, nr_components = label(img_reshaped, return_num=True, connectivity=3)
 
+    # 2.3 closer look at single components or avg of 1/2/3/4...
+    # area 
+    # "anti-convexity" of cc (conv_hull-cc)/area
+    # eccentricity
+    # euler_number
+    # major axis length
+    # mean_intensity(how to get intensities in function?)
+
     
     # 3. Compute volumes 
 
@@ -74,11 +89,18 @@ def compute_metrics(seg_path):
     volume_left = volume - volume_right
     ratio_left_right_lung = volume_left/volume_right
 
+
+    #4. Compare to more trivial segmentation if possible. Maybe take otsu with pixel value of consolidated area(from centroid intensity of biggest cc) or smth alike 
+
+
+
+# Histogram over all values, then decide on interesting values
+
+
     
 
 
-
-# get the images and into numpy
+# get the images and into numpy in order to test a bit
 global_name = "VESSEL12"
 dataset_path = os.path.join(storage_data_path, global_name)
 study_names = set(file_name.split('.nii')[0].split('_gt')[0] for file_name in os.listdir(dataset_path))
---
+++ experiments_quality_assurance_UK_Frankfurt2.py
@@ -0,0 +1,70 @@
+## Dimensionality reduction and visualisation of each segmented region 
+# First of all, the images are resized to the same dimesionality (?,256,256)
+# Then, each component, that is big enough (we start with size>500), is taken and we feed all of them into the dim. Reduction.
+
+# 1. Imports 
+import os 
+import SimpleITK as sitk 
+import numpy as np 
+import torchio
+import torch
+from mp.data.pytorch.transformation import resize_3d
+from skimage.measure import label,regionprops
+from torch.nn.functional import interpolate
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt 
+import pickle 
+
+# 2. reduce dimension of images to (57,256,256) and load components into an array 
+seg_comp = []
+for dir in os.listdir(os.path.join('downloads','UK_Frankfurt2')):
+    path = os.path.join('downloads','UK_Frankfurt2',dir)
+    img_path = os.path.join(path,'image.nii.gz')
+    seg_path = os.path.join(path,'mask.nii.gz')
+    img = torch.tensor(torchio.Image(img_path, type=torchio.INTENSITY).numpy())
+    seg = torch.tensor(torchio.Image(seg_path, type=torchio.LABEL).numpy())
+    
+    #2.1 resize 
+    img = resize_3d(img, size=(1,256,256,57))
+    seg = resize_3d(seg, size=(1,256,256,57), label=True)
+    img = img.numpy()[0]
+    seg = seg.numpy()[0]
+    
+    shape = np.shape(seg)
+    components = label(seg)
+    props = regionprops(components,img)
+    props = sorted(props ,reverse=True, key =lambda dict:dict['area'])
+    number_components = len(props)
+
+    # 2.2 read out the components
+    comp = 0
+    while props[comp].area > 500 and comp < number_components : 
+        coords = props[comp].coords
+        component_mask = np.zeros(shape)
+        for x,y,z in coords: 
+            component_mask[x,y,z] = 1
+        component_mask = component_mask.flatten()
+        seg_comp.append(component_mask)
+
+
+# 3. transform fit this array into 2 dim 
+transformer = TruncatedSVD(n_iter=10,random_state=34)
+transformer.fit_transform(seg_comp)
+plt.scatter(seg_comp[:,0],seg_comp[:,1])
+plt.savefig(os.path.join('storage','statistics','UK_Frankfurt2','dim_reduced_UK_Frankfurt2.png'))
+pickle.dump(transformer,open(os.path.join('storage','statistics','UK_Frankfurt2','transformer_UK_Frankfurt2.sav'),'wb'))
+pickle.dump(seg_comp,open(os.path.join('storage','statistics','UK_Frankfurt2','trans_data_UK_Frankfurt2.sav'),'wb'))
+
+
+
+# 5. plot the result 
+
+
+
+
+
+
+
+
+
+
