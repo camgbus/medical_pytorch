94c8739840f831f173322c670c26003bd12aad62
CrispyHarder <christian.harder96@gmail.com>
2021-03-26 18:10:29 +0100

    this file is for tests and changed all the time; commiting so i can pull

--- seg_qual_JIP_tests.py
+++ seg_qual_JIP_tests.py
@@ -6,7 +6,7 @@ from mp.utils.feature_extractor import Feature_extractor
 from mp.models.densities.density import Density_model
 #set environmental variables
 os.environ["WORKFLOW_DIR"] = os.path.join(JIP_dir, 'data_dirs')
-os.environ["OPERATOR_IN_DIR"] = "input_small"
+os.environ["OPERATOR_IN_DIR"] = "input"
 os.environ["OPERATOR_OUT_DIR"] = "output"
 os.environ["OPERATOR_TEMP_DIR"] = "temp"
 os.environ["OPERATOR_PERSISTENT_DIR"] = os.path.join(JIP_dir, 'data_dirs', 'persistent')
@@ -15,13 +15,13 @@ def test_train_density_working():
     
     #set the params 
     model='gaussian_kernel'
-    ending_of_model='dummy'
-    list_of_paths=[os.path.join(os.environ['WORKFLOW_DIR'],os.environ['OPERATOR_IN_DIR'])]
-    data_mode = 'normal'
-    data_describtion = '3 img-seg pairs from UK Frankfurt'
+    ending_of_model='UK_Fra'
+    list_of_paths=[]#[os.path.join(os.environ['WORKFLOW_DIR'],os.environ['OPERATOR_IN_DIR'])]
+    data_mode = 'JIP_test'
+    data_describtion = 'all 30 img-seg pairs from UK Frankfurt, new pairs, new data format'
     model_describtion = 'gaussian_kernel with bw 0.005'
-    precom_intensities = []# ['dummy_int']
-    verbose = True 
+    precom_intensities = ['UK_Fra']# ['dummy_int']
+    verbose = False 
     #test, if density model works
     train_density(model,ending_of_model,list_of_paths,data_mode,
                     data_describtion,model_describtion,precom_intensities,verbose, bandwidth=0.005)
@@ -29,19 +29,24 @@ def test_train_density_working():
     print('Everything went through, so should be fine')
 
 def test_get_intensities_working():
+    mode = 'JIP_test'
+    save = True 
+    save_name = 'UK_Fra'
+    save_descr = 'Intensities of all 30 img-seg pairs in UK_Fra'
     list_of_paths=[os.path.join(os.environ['WORKFLOW_DIR'],os.environ['OPERATOR_IN_DIR'])]
-    get_intensities(list_of_paths, min_size=100, mode='normal',save = False, save_name=None, save_descr=None)
+    verbose = True
+    get_intensities(list_of_paths, min_size=100, mode=mode,save = save, save_name=save_name, save_descr=save_descr, verbose=True)
     print('Everything went through, so should be fine')
 
 def test_feature_extractor_working():
     list_of_paths=[os.path.join(os.environ['WORKFLOW_DIR'],os.environ['OPERATOR_IN_DIR'])]
-    save = False
-    save_name='dummy_features'
-    save_descr='3 img-seg pairs from UK Fra'
-    mode = 'normal'
-    features = ['density_distance'] #['density_distance','dice_scores','connected_components']
+    save = True
+    save_name='UK_Fra_density_distances'
+    save_descr='all 30 img-seg pairs from UK Fra. Computed averaged density distances'
+    mode = 'JIP_test'
+    features = ['density_distance','dice_scores','connected_components']
 
-    density = Density_model(add_to_name='dummy')
+    density = Density_model(add_to_name='UK_Fra')
     
     feat_extr = Feature_extractor(density=density,features=features)
     feat_arr = feat_extr.get_features_from_paths(list_of_paths,mode=mode,save=save,
@@ -49,8 +54,10 @@ def test_feature_extractor_working():
     print(feat_arr)
     print('Everything went through, so should be fine')
 
+#test_get_intensities_working()
+#test_train_density_working()
 test_feature_extractor_working()
-    
+
 
 
     
