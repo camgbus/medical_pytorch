22391aa67a4aaa9d1ba0a55921cc76bd0e5fdb59
CrispyHarder <christian.harder96@gmail.com>
2021-04-10 17:08:13 +0200

    adapted code to other changes, now easier to use, since no direct input is needed;

--- train_restore_use_models/train_int_based_quantifier.py
+++ train_restore_use_models/train_int_based_quantifier.py
@@ -11,24 +11,24 @@ def train_int_based_quantifier(preprocess=True,train_dens=True,train_dice_pred=T
     if os.environ["INFERENCE_OR_TRAIN"] == 'train':
         if preprocess:
             if verbose:
-                print('Preprocessing data ...', end="\r")
+                print('Preprocessing data ...')
             basic_preprocessing() 
         if train_dens:
             if verbose:
-                print('Training density ...', end="\r")
-            train_density(verbose = verbose , bandwidth=0.005)
+                print('Training density ...')
+            train_density(verbose=verbose)
         if train_dice_pred:
             if verbose:
-                print('Training dice predictor ...', end="\r")
+                print('Training dice predictor ...')
             extract_features_all_data()
             compute_all_prediction_dice_scores()
-            train_dice_predictor(verbose=verbose,solver='adam',learning_rate='adaptive',hidden_layer_sizes=(10,30,50,50,20))
+            train_dice_predictor(verbose=verbose)
     else : 
         print('This is only for Train mode')
 
         
-def train_density(model = 'gaussian_kernel',
-                    ending_of_model = 'standart',
+def train_density(model = '',
+                    ending_of_model = '',
                     list_of_paths = [os.path.join(os.environ["PREPROCESSED_WORKFLOW_DIR"],os.environ["PREPROCESSED_OPERATOR_OUT_SCALED_DIR_TRAIN"])],
                     data_mode='JIP',
                     data_describtion = 'training on nearly all data from COVID-RACOON at 5.4.2021', 
@@ -38,7 +38,6 @@ def train_density(model = 'gaussian_kernel',
                     **kwargs):
     '''Trains a density model from a list of given paths to directories, where img-seg pairs can be found
         and stores it
-
     Args:
         model (str): The name of the model being trained. Gives the first part of the name
         ending_of_model (str): in order to specify model with name 'model' shall be trained ;
@@ -50,14 +49,22 @@ def train_density(model = 'gaussian_kernel',
             which dataset, when the training was done, etc)
         model_describtion (str): a string to describe the specifications of the density model (e.g. bandwith, 
             other parameters, etc)
-        precom_intesities (list(str)): a list of name of already computed intensity values, that are loaded 
+        precom_intensities (list(str)): a list of name of already computed intensity values, that are loaded 
             from op_pers_dirs/intensities, dont need .npy ending
         verbose (bool): If consolte outputs should be printed
         **kwargs is used for the specifics of the model to be trained
     '''
 
     #initialise density model
-    density_model = Density_model(model=model,add_to_name=ending_of_model,verbose=verbose)
+    if model and ending_of_model:
+        density_model = Density_model(model=model,add_to_name=ending_of_model,verbose=verbose)
+    if model and not ending_of_model:
+        density_model = Density_model(model=model,verbose=verbose)
+    if not model and ending_of_model:
+        density_model = Density_model(add_to_name=ending_of_model,verbose=verbose)
+    if not model and not ending_of_model:
+        density_model = Density_model(verbose=verbose)
+    
 
     #get the intensity values from the images 
     if verbose:
@@ -79,25 +86,19 @@ def train_density(model = 'gaussian_kernel',
         density_model.plot_density()
 
 
-def train_dice_predictor(model_name='standart',feature_extractor=Feature_extractor(),data_describtion = 'all of train data',
+def train_dice_predictor(model_name='standart',feature_extractor=None,data_describtion = 'all of train data',
                             model_describtion = 'MLP',verbose=False,**kwargs):
     '''Trains a dice predictor model based on features extracted from image-seg pairs
-     a standart setting for kwargs is: 
-     kwargs = {'solver'='adam','lr'='adaptive','hidden_layer_sizes'=(10,30,50,50,20)}
 
     Args:
         model_name (str): The name of the model
-        feature_names (list(str)):The names of the features to be used
-        dens_model (str): The first part of the density model name 
-        dens_add_name (str): the second part of the density model name 
-            specifying the exact model 
-        list_of_paths (list(str)): a list of paths to the directories, where features
-            shall be extracted from 
-        names_extracted_features (list(str)): names of already extracted features, that are loaded directly 
-        y_train (ndarray): the label vector
+        feature_extractor (instance of Feature_extractor class):The names of the features to be used
         data_describtion (str): a describtion of the used data
         model_describtion (str): a describtion of the used model and density model
+        verbose(bool): whether the model shall print output
     '''
+    if not feature_extractor:
+        feature_extractor = Feature_extractor()
 
     #initiate model
     dice_pred = Dice_predictor(features=feature_extractor.features,add_to_name=model_name,verbose=verbose)
